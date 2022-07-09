92eb11b4452f2cb59559294329a3a932f9fbe1e4
Tobias Verleger <tobias.verleger@kalera.com>
2022-07-09 08:27:40 +0200

    added docstrings

--- helper.py
+++ helper.py
@@ -3,8 +3,10 @@ import numpy as np
 
 
 def get_number_of_zero_values(column: Series):
+    '''takes a series and returns the number of zero entries in that column'''
     return column.size -np.count_nonzero(column)
 
 
 def get_city_or_region(df: DataFrame, city:str): 
+    '''takes a dataframe and a string and returns the dataframe filtered by the string'''
     return df[df.geo_bln ==city]
